240be652ae33b8bf462ac9fcc7114af73bf031e6
Gibson <raycoutley646@gmail.com>
2025-09-11 11:06:51 -0600

    added author section

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -9,6 +9,7 @@ import nftImage from "../../images/nftImage.jpg";
 const NewItems = () => {
   const [newItems, setNewItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentTime, setCurrentTime] = useState(Date.now());
 
   useEffect(() => {
     const fetchNewItems = async () => {
@@ -61,10 +62,18 @@ const NewItems = () => {
     fetchNewItems();
   }, []);
 
+  // Update timer every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(Date.now());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   const formatTimeLeft = (expiryDate) => {
-    const now = new Date().getTime();
     const expiry = new Date(expiryDate).getTime();
-    const timeLeft = expiry - now;
+    const timeLeft = expiry - currentTime;
 
     if (timeLeft <= 0) {
       return "Expired";
